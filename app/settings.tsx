5e20b6a28fd3c3644197e61099695e0b5356daf7
Aquel32 <aquelyt@gmail.com>
2025-02-26 19:12:26 +0100

    Settings GUI

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,5 +1,30 @@
+import CustomButton from "@/components/CustomButton";
 import { View } from "react-native";
 
 export default function SettingsPage() {
-  return <View className="bg-primary h-full"></View>;
+  return (
+    <View className="bg-primary h-full items-center p-10 gap-10">
+      <CustomButton
+        title={"Importuj dane"}
+        handlePress={() => {}}
+        containerStyles={"bg-slate-400 px-5"}
+        textStyles={""}
+        isLoading={false}
+      />
+      <CustomButton
+        title={"Eksportuj dane"}
+        handlePress={() => {}}
+        containerStyles={"bg-slate-400 px-5"}
+        textStyles={""}
+        isLoading={false}
+      />
+      <CustomButton
+        title={"Reset do ustawień fabrycznych"}
+        handlePress={() => {}}
+        containerStyles={"bg-red-400 px-5"}
+        textStyles={"text-black font-bold"}
+        isLoading={false}
+      />
+    </View>
+  );
 }
